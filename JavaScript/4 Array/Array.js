b1e244c5ba0a05b57b4a3e2fb215e635c613c19f
Naresh Kumar Mohanan <nareshmohanan@gmail.com>
2025-09-02 17:50:16 +0530

    Addes Some Js Toppics in Arrays.js file

--- JavaScript/4 Array/Array.js
+++ JavaScript/4 Array/Array.js
@@ -68,3 +68,86 @@ console.log(arr6);
 // 10. join()
 let arr7 = ["Hello", "Hi", "How", "are", "you"]
 console.log(arr7.join(" "));
+
+
+
+// ! Reverse the string using only methods
+let str = "hello"
+
+let arr8 = str.split("");
+console.log(arr8);
+
+let revarr = arr8.reverse();
+console.log(revarr);
+
+/* let revstr = revarr.join("");
+console.log(revstr);
+console.log(str.split("").reverse().join("")); */
+
+
+
+// ! 11. Slice()
+// It only take the values from th array.
+let arr9 = [8, 10, 40, 66, 30, 22, 10];
+console.log(arr9.slice(2, 5 + 1)); // slice(Start, End)
+
+
+// ! 12. Splice()
+// It modify the Original Array and Remove the element.  
+let arr10 = [8, 10, 40, 66, 30, 22, 10];
+arr10.splice(2, 3, 400); // splice(Start, Remove Element Count, Replace Word or Number anything)
+console.log(arr10);
+
+let subjects = ["html", "css", "js"];
+subjects.splice(3, 0, "SQL")
+console.log(subjects);
+
+
+// ! Higher Order Array Methods
+
+// ! 1. Map()
+let number = [80, 75, 60, 82, 92]  // o/p --> [85,80,65,87,97]
+let x = [];
+for (i = 0; i < number.length; i++) {
+    // x[i] = number[i] + 5;
+    x.push(number[i] + 5);
+}
+console.log(x);
+
+let mappedarr = number.map((element) => {
+    return element + 5;
+})
+console.log(mappedarr);
+
+let subject = ["html", "css", "java", "js"]
+let uppercasearr = subject.map((subjects) => {
+    return subjects.toUpperCase();
+})
+console.log(uppercasearr);
+
+
+// ! 2. Filter()
+let greater = number.filter((nums) => {
+    return nums > 80;
+})
+console.log(greater);
+
+
+// ! 3. Reduce()
+let arr11 = [10, 20, 30, 40, 50, 60];
+let sum = arr11.reduce((accumulator, element) => {
+    return accumulator + element;
+}/* (,Accumulator Value will be there) */)
+console.log(sum);
+
+
+// ! 4. Sort()
+let arr12 = [3, 1, 6, 22, 5, 9]
+let ascOrder = arr12.sort((a, b) => {
+    return a - b; // a-b Will give Assending order of data 
+})
+console.log(ascOrder);
+let dscOrder = arr12.sort((a, b) => {
+    return b - a; // b-a Will give the Dessending Order of data
+})
+console.log(dscOrder);
