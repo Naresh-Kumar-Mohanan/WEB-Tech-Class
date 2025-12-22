b1cde15869838efe54bec247dd0414c70b532590
Naresh Kumar Mohanan <nareshmohanan@gmail.com>
2025-12-22 17:47:26 +0530

    react installation

---
+++ React/basics/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h2>This React Class</h2>
+        <h3>React Project Creation</h3>
+      </div>
+    </>
+  )
+}
+
+export default App
