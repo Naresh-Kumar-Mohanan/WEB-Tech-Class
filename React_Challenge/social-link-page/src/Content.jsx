9a8dbec024aa5ffd60a8ac81c1f93c7d56d21a66
Naresh Kumar Mohanan <nareshmohanan@gmail.com>
2026-01-05 19:57:31 +0530

    task done

--- React_Challenge/social-link-page/src/Content.jsx
+++ React_Challenge/social-link-page/src/Content.jsx
@@ -1,8 +1,21 @@
 import avatarImg from "/avatar-jessica.jpeg"
+import "./Content.css"
 
 let Content = () => {
-    return <div className="content">
-        <img src={avatarImg} alt="" />
+    return <div className="outerLayer">
+        <div className="content">
+            <img src={avatarImg} alt="" />
+            <h2>Jessica Randall</h2>
+            <p id="p1">London, United Kingdom</p>
+            <p>"Front-end developer and avid reader"</p>
+            <div className="tags">
+                <button>GitHub</button>
+                <button>Frontend Mentor</button>
+                <button>Linkedin</button>
+                <button>Twitter</button>
+                <button>Instagram</button>
+            </div>
+        </div>
     </div>
 }
 
