f0216978255f75f68b4ff51794a8608683ea8d57
Naresh Kumar Mohanan <nareshmohanan@gmail.com>
2026-01-06 15:41:43 +0530

    commit

--- React/props/src/Child.jsx
+++ React/props/src/Child.jsx
@@ -1,19 +1,19 @@
-export default function Parent(anyName){
+export default function Parent(anyName) {
 
     console.log(anyName);
-    
-    return <div>
-       <h2> Hellooo!!</h2>
-       <ul>
-       <h2>{anyName.fam.map((e,i)=>{
-        return <li key = {i}>{e} </li>
-       })}</h2>
-       </ul>
 
-      
-      <h2>User Name: {anyName.me.name}</h2>
-       <h2>Email: {anyName.me.email}</h2>
-       {anyName.children}
+    return <div>
+        <h2> Hellooo!!</h2>
+        <ul>
+            <h2>{anyName.fam.map((e, i) => {
+                return <li key={i}>{e} </li>
+            })}</h2>
+        </ul>
+
+
+        <h2>User Name: {anyName.me.name}</h2>
+        <h2>Email: {anyName.me.email}</h2>
+        {anyName.children}
 
 
 
--- React/props/src/Parent.jsx
+++ React/props/src/Parent.jsx
@@ -1,25 +1,26 @@
 import Child from "./Child"
 
-export default function Parent(props){
- console.log(props);
+export default function Parent(props) {
+    console.log(props);
 
- let arr = ["Naresh","Kumar","Ajay","Kalai"]
+    let arr = ["Naresh", "Kumar", "Ajay", "Kalai"]
 
- let obj = {name:"Naresh",
-            email:"naresh@gmail.com",
- }
+    let obj = {
+        name: "Naresh",
+        email: "naresh@gmail.com",
+    }
 
     return <div>
-       <h3> I am from Parent Component but {props.abc}</h3>
-       <h3>Sum of {props.a} and {props.b} is {props.a + props.b} </h3>
+        <h3> I am from Parent Component but {props.abc}</h3>
+        <h3>Sum of {props.a} and {props.b} is {props.a + props.b} </h3>
 
 
-<Child fam ={arr} me ={obj} >
+        <Child fam={arr} me={obj} >
 
-    <form action="">
-        <input type="text" /><input type="text" />
-    </form>
-</Child>
-       
+            <form action="">
+                <input type="text" /><input type="text" />
+            </form>
+        </Child>
+
     </div>
 }
